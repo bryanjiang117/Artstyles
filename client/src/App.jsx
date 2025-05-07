b37b26a7453503b05c7e27f5f00e980a8ad7148b
Bryan Jiang <haozhoujiang@gmail.com>
2025-05-07 01:19:09 -0400

    Integrate artwork API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import dotenv from 'dotenv'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ artworks, setArtworks ] = useState([])
+
+  dotenv.config()
+
+  useState(async () => {
+    const url = `${process.env.API_BASE}/api/artworks`
+    try { 
+      const response = await fetch(url)
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+
+      const data = await response.json()
+      console.log(data)
+      setArtworks(data)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='gallery'>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
