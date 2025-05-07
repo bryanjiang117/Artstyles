b303ed9bee0f0f092bb25a5e91693b9a7e608fdb
Bryan Jiang <haozhoujiang@gmail.com>
2025-05-07 01:48:57 -0400

    Display artwork

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [ artworks, setArtworks ] = useState([])
+  const [artworks, setArtworks] = useState([]);
 
-  useState(async () => {
-    const url = `${import.meta.env.VITE_API_BASE}/api/artworks`
-    try { 
-      const response = await fetch(url)
+  useEffect(() => {
+    async function fetchData() {
+      const url = `${import.meta.env.VITE_API_BASE}/api/artworks`;
+      try {
+        const response = await fetch(url);
 
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`)
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data[0].preview_url);
+        setArtworks(data.slice(0, 101));
+      } catch (error) {
+        console.error(error.message);
       }
-
-      const data = await response.json()
-      console.log(data)
-      setArtworks(data)
-    } catch (error) {
-      console.error(error.message)
     }
-  }, [])
+    fetchData()
+  }, []);
 
   return (
-    <>
-      <div className='gallery'>
-
+    <div className="main">
+      <div className="gallery">
+        {artworks.map((artwork, index) => 
+          <img src={artwork.preview_url} alt="" key={index}/>
+        )}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
