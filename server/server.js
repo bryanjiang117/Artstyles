bf75ce5c6dc49699ddc8dff1ff3307085241f0a8
Bryan Jiang <haozhoujiang@gmail.com>
2025-05-06 23:44:05 -0400

    Express endpoint for retrieving list of art

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+import pgPromise from 'pg-promise'
+import express from 'express'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+
+const pgp = pgPromise()
+const db = pgp(process.env.DATABASE_URL)
+
+app.get('/api/artworks', async (req, res) => {
+  try {
+    const data = await db.one(`
+      SELECT permalink, preview_url 
+      FROM artworks   
+    `)
+
+    console.log('DATA!!', data)
+    res.json(data)
+  } catch (error) {
+    console.log('Failed to fetch artworks.', error)
+    res.status(500).send('Database error while fetching artworks')
+  }
+})
